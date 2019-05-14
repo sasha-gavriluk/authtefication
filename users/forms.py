e6a82fabbfba969f6d228983a14186cdc493af07
sasha-gavriluk <killer.sasha88@gamil.com>
2019-05-14 23:19:46 +0300

    Auth Commit

--- users/forms.py
+++ users/forms.py
@@ -6,10 +6,10 @@ class CustomUserCreationForm(UserCreationForm):
     
     class Meta(UserCreationForm.Meta):
         model = CustomUser
-        fields = UserCreationForm.Meta.fields
+        fields = ('username', 'email')
 
 class CustomUserChangeForm(UserChangeForm):
     
     class Meta(UserChangeForm.Meta):
         model = CustomUser
-        fields = UserChangeForm.Meta.fields
+        fields = ('username', 'email')
